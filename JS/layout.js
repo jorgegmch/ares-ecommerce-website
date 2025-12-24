38aea6b0e7f6317f9eb9dc669452e396240243fa
jorgegmch <226594601+jorgegmch@users.noreply.github.com>
2025-12-24 17:11:28 -0500

    feat: se completa organización y funcionalidad del nav en el index para desktop

--- JS/layout.js
+++ JS/layout.js
@@ -1,1 +1,25 @@
 // Logica back-end del NAV responsive
+
+const menuButton = document.querySelector('.menu-responsive');
+const overlay = document.querySelector('.menu-overlay');
+const closeButton = document.querySelector('.menu-overlay-close');
+
+// Abrir / cerrar menú
+function toggleMenu() {
+    menuButton.classList.toggle('active');
+    overlay.classList.toggle('active');
+}
+
+function closeMenu() {
+    menuButton.classList.remove('active');
+    overlay.classList.remove('active');
+}
+
+menuButton.addEventListener('click', toggleMenu);
+
+closeButton.addEventListener('click', closeMenu);
+
+
+document.querySelectorAll('.main-menu a').forEach(link => {
+    link.addEventListener('click', closeMenu);
+});
